c11db0632d60a6de3f7253085ba9c536ba50e7f9
JD_Zar <diegorayau@hotmail.com>
2025-08-24 17:59:39 -0500

    Actualización de proyecto 364

--- fronted/src/app/layout.tsx
+++ fronted/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/sonner";
@@ -11,16 +10,6 @@ import ChatButton from "@/components/ChatButton";
 import { MessagesProvider } from "@/context/messages-context";
 
 export const dynamic = "force-dynamic";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -34,9 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
