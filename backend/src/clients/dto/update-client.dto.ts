17aabafef28abcf825c3ae965eff874a0aa648e6
JD_Zar <diegorayau@hotmail.com>
2025-10-14 16:12:12 -0500

    test06

--- backend/src/clients/dto/update-client.dto.ts
+++ backend/src/clients/dto/update-client.dto.ts
@@ -2,4 +2,7 @@ import { CreateClientDto } from "./create-client.dto";
 
 //export class UpdateProductDto extends PartialType(CreateProductDto) {}
 
-export type UpdateClientDto = Partial<CreateClientDto> & { id: number };
+export type UpdateClientDto = Partial<CreateClientDto> & {
+  id: number;
+  organizationId?: number | null;
+};
